9096b6dd341085411cf4e23fdee3f9da4d70e776
eric-mendoza <men15002@uvg.edu.gt>
2019-04-18 22:44:54 -0600

    Image gets scaled before getting saved

--- canvas.py
+++ canvas.py
@@ -2,7 +2,7 @@
 #
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QAction, QFileDialog
-from PyQt5.QtGui import QIcon, QImage, QPainter, QPen
+from PyQt5.QtGui import QIcon, QImage, QPainter, QPen, QPixmap
 from PyQt5.QtCore import Qt, QPoint
 import sys
 
@@ -13,57 +13,60 @@ class Window(QMainWindow):
         super().__init__()
 
         # Set initial values for window
-        top = 400
-        left = 400
+        top = 200
+        left = 200
         width = 800
-        height = 600
+        height = 800
         self.setWindowTitle("Painter")
         self.setGeometry(top, left, width, height)
+
+        # Crete canvas for drawing
         self.image = QImage(self.size(), QImage.Format_RGB32)
         self.image.fill(Qt.white)
+
         self.drawing = False
-        self.brushSize = 5
+        self.brushSize = 80
         self.brushColor = Qt.black
         self.lastPoint = QPoint()
 
         # Create main menu
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu("File")
-        brushMenu = mainMenu.addMenu("Brush Size")
-        brushColor = mainMenu.addMenu("Brush Color")
+        main_menu = self.menuBar()
+        file_menu = main_menu.addMenu("File")
+        brush_menu = main_menu.addMenu("Brush Size")
+        brush_color = main_menu.addMenu("Brush Color")
 
         # Add options to 'File' menu
-        saveAction = QAction("Save", self)
-        saveAction.setShortcut("Ctrl+S")
-        fileMenu.addAction(saveAction)
-        saveAction.triggered.connect(self.save)
+        save_action = QAction("Save", self)
+        save_action.setShortcut("Ctrl+S")
+        file_menu.addAction(save_action)
+        save_action.triggered.connect(self.save)
 
-        clearAction = QAction("Clear", self)
-        clearAction.setShortcut("Ctrl+C")
-        fileMenu.addAction(clearAction)
-        clearAction.triggered.connect(self.clear)
+        clear_action = QAction("Clear", self)
+        clear_action.setShortcut("Ctrl+C")
+        file_menu.addAction(clear_action)
+        clear_action.triggered.connect(self.clear)
 
         # Add options to 'Brush' menu
-        threepxAction = QAction("3px", self)
-        threepxAction.setShortcut("Ctrl+3")
-        brushMenu.addAction(threepxAction)
+        threepx_action = QAction("3px", self)
+        threepx_action.setShortcut("Ctrl+3")
+        brush_menu.addAction(threepx_action)
 
-        fivepxAction = QAction("5px", self)
-        fivepxAction.setShortcut("Ctrl+5")
-        brushMenu.addAction(fivepxAction)
+        fivepx_action = QAction("5px", self)
+        fivepx_action.setShortcut("Ctrl+5")
+        brush_menu.addAction(fivepx_action)
 
-        sevenpxAction = QAction("7px", self)
-        sevenpxAction.setShortcut("Ctrl+7")
-        brushMenu.addAction(sevenpxAction)
+        sevenpx_action = QAction("7px", self)
+        sevenpx_action.setShortcut("Ctrl+7")
+        brush_menu.addAction(sevenpx_action)
 
-        ninepxAction = QAction("9px", self)
-        ninepxAction.setShortcut("Ctrl+9")
-        brushMenu.addAction(ninepxAction)
+        ninepx_action = QAction("9px", self)
+        ninepx_action.setShortcut("Ctrl+9")
+        brush_menu.addAction(ninepx_action)
 
         # Add options to 'Brush Color' menu
-        blackAction = QAction("Black", self)
-        blackAction.setShortcut("Ctrl+B")
-        brushColor.addAction(blackAction)
+        black_action = QAction("Black", self)
+        black_action.setShortcut("Ctrl+B")
+        brush_color.addAction(black_action)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -83,14 +86,15 @@ class Window(QMainWindow):
             self.drawing = False
 
     def paintEvent(self, event):
-        canvasPainter = QPainter(self)
-        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+        canvas_painter = QPainter(self)
+        canvas_painter.drawImage(self.rect(), self.image, self.image.rect())
 
     def save(self):
-        filePath, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "BMP(*.bmp);; ALL Files(*.*)")
-        if filePath == "":
+        file_path, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "BMP(*.bmp);; ALL Files(*.*)")
+        if file_path == "":
             return
-        self.image.save(filePath)
+        new = self.image.scaled(28, 28)  # Escalar la imagen
+        new.save(file_path)
 
     def clear(self):
         self.image.fill(Qt.white)
